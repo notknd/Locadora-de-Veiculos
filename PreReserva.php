0e88a2c7a242132bef1eb0cf98becdc014f2e058
devRaissa <kaoriraissa87@gmail.com>
2025-11-15 15:05:49 -0300

    Criação da classe Pre Reserva e trabalhando a interatividade no index.

--- PreReserva.php
+++ PreReserva.php
@@ -4,19 +4,26 @@ namespace App;
 
 use App\Model\Cliente;
 use App\Model\Veiculo;
+use App\Model\PagamentoPix;
 
 class PreReserva{
+    private PagamentoPix $pix;
     private Cliente $cliente;
     private Veiculo $veiculo;
     private string $prazo;
     private string $veiculo_pre;
+    private int $metodoPagamento;
+    
 
-    public function __construct($cliente, string $veiculo_pre, string $prazo){
+    public function __construct(Cliente $cliente, string $veiculo_pre, string $prazo, int $metodoPagamento){
         $this->cliente = $cliente;
         $this->setPrazo($prazo);
         $this->setVeivuloPre($veiculo_pre);
+        $this->metodoPagamento = $metodoPagamento;
     }
-
+    public function getPagamentoPix($pix){
+        $this->pix = $pix;
+    }
     public function getPrazo():string{
         return $this->prazo;
     }
@@ -30,10 +37,14 @@ class PreReserva{
     protected function setVeivuloPre(string $veiculo_pre){
         $this->veiculo_pre = $veiculo_pre;
     }
+
     public function exibir_detalhes(){
         $this->cliente->exibirDetalhes();
         $this->veiculo->exibirDetalhes();
         $this->getPrazo();
         $this->getVeiculoPre();
+        if (processarPagamento() == true){
+            echo "Pré reserva efetuado com sucesso!";
+        }
     }
 }
