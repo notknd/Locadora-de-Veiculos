d391f53c82512d3d1f64c8bedf944e7a8c51dd82
JoaoPAAguiar <joaopedroaguiar0707@gmail.com>
2025-11-24 19:52:41 -0300

    Atualizando os erros identificados no teste

--- index.php
+++ index.php
@@ -16,9 +16,9 @@ $veiculos = [];
 $clientes = [];
 $alugueis = [];
 
-echo "________________________________________\n";
+echo "______________\n";
 echo "  Bem-vindo à Locadora de Veículos POO  \n";
-echo "________________________________________\n";
+echo "______________\n";
 
 while (true) {
     echo "\nEscolha uma opção:\n";
@@ -37,7 +37,7 @@ while (true) {
 
     switch ($opcao) {
         case 1:
-            echo "___ Cadastro de Carro ___\n";
+            echo "_ Cadastro de Carro _\n";
             echo "Placa: ";
             $placa = readline();
             echo "Marca: ";
@@ -56,7 +56,7 @@ while (true) {
             break;
 
         case 2:
-            echo "___ Cadastro de Moto ___\n";
+            echo "_ Cadastro de Moto _\n";
             echo "Placa: ";
             $placa = readline();
             echo "Marca: ";
@@ -75,7 +75,7 @@ while (true) {
             break;
 
         case 3:
-            echo "___ Cadastro de Cliente ___\n";
+            echo "_ Cadastro de Cliente _\n";
             echo "CPF: ";
             $cpf = readline();
             echo "Nome: ";
@@ -91,159 +91,198 @@ while (true) {
             break;
 
         case 4:
-            echo "___ Lista de Veículos ___\n";
-            foreach ($veiculos as $veiculo) {
-                $veiculo->exibirDetalhes();
+            if(empty($veiculos)){
+                echo "Realiza o cadastro do veiculo!\n";
+                break;
             }
-            break;
-
-        case 5:
-            echo "___ Lista de Clientes ___\n";
-            foreach ($clientes as $cliente) {
-                $cliente->exibirDetalhes();
-            }
-            break;
-
-        case 6:
-            echo "___ Registro de Novo Aluguel ___\n";
-
-            echo "Escolha um veículo da lista abaixo:\n";
-            foreach ($veiculos as $indice => $veiculo) {
-                echo ($indice + 1) . ". ";
-                $veiculo->exibirDetalhes();
-            }
-            echo "Digite o NÚMERO do veículo: ";
-            $indiceVeiculo = (int) readline() - 1;
-            $veiculoEscolhido = $veiculos[$indiceVeiculo];
-
-            echo "\nEscolha um cliente da lista abaixo:\n";
-            foreach ($clientes as $indice => $cliente) {
-                echo ($indice + 1) . ". ";
-                $cliente->exibirDetalhes();
-            }
-            echo "Digite o NÚMERO do cliente: ";
-            $indiceCliente = (int) readline() - 1;
-            $clienteEscolhido = $clientes[$indiceCliente];
-
-            echo "Digite a data de início do aluguel: ";
-            $dataInicio = readline();
-
-            $novoAluguel = new Aluguel($clienteEscolhido, $veiculoEscolhido, $dataInicio);
-            $alugueis[] = $novoAluguel;
-
-            echo "\nAluguel registrado com sucesso!\n";
-            $novoAluguel->exibirDetalhes();
-
-
-            echo "Digite o NÚMERO do método de pagamento: ";
-            echo "\n1. Cartão\n2. Boleto\n3.Pix\n";
-            $indicePag = (int) readline() - 1;
-            $metodoPagamento = $indicePag + 1; // 1: cartão, 2: boleto, 3: pix
-
-
-            if ($metodoPagamento === 1) {
-                $pagamento = new PagamentoCartao($valorPreReserva, contaCliente: $clienteEscolhido->getConta());
-                $sucessoPagamento = $pagamento->processar_pagamento();
-
-            } elseif ($metodoPagamento === 2) {
-                $pagamento = new PagamentoBoleto($valorPreReserva, $clienteEscolhido->getConta());
-                $sucessoPagamento = $pagamento->processar_pagamento();
-
-            } elseif ($metodoPagamento === 3) {
-                $pagamento = new PagamentoPix($valorPreReserva, $clienteEscolhido->getConta());
-                $sucessoPagamento = $pagamento->processar_pagamento();
-
-            } else {
-                echo "Método de pagamento inválido.\n";
+            else{
+                echo "_ Lista de Veículos _\n";
+                foreach ($veiculos as $veiculo) {
+                    $veiculo->exibirDetalhes();
+                }
                 break;
             }
 
-        case 7:
-            echo "___ Lista de Aluguéis ___\n";
-            foreach ($alugueis as $aluguel) {
-                $aluguel->exibirDetalhes();
+        case 5:
+            if(empty($clientes)){
+                echo "Realiza o cadastro de cliente!\n";
+                break;
             }
-            break;
+            else{
+                echo "_ Lista de Clientes _\n";
+                foreach ($clientes as $cliente) {
+                    $cliente->exibirDetalhes();
+                }
+                break;
+            }
+
+        case 6:
+            if(empty($clientes)){
+                echo "Realiza o cadastro de cliente!\n";
+                break;
+            }
+            elseif(empty($veiculos)){
+                echo "Realiza o cadastro do veiculo!\n";
+                break;
+            }
+            else{
+                echo "_ Registro de Novo Aluguel _\n";
+
+                echo "Escolha um veículo da lista abaixo:\n";
+                foreach ($veiculos as $indice => $veiculo) {
+                    echo ($indice + 1) . ". ";
+                    $veiculo->exibirDetalhes();
+                }
+                echo "Digite o NÚMERO do veículo: ";
+                $indiceVeiculo = (int) readline() - 1;
+                $veiculoEscolhido = $veiculos[$indiceVeiculo];
+
+                echo "\nEscolha um cliente da lista abaixo:\n";
+                foreach ($clientes as $indice => $cliente) {
+                    echo ($indice + 1) . ". ";
+                    $cliente->exibirDetalhes();
+                }
+                echo "Digite o NÚMERO do cliente: ";
+                $indiceCliente = (int) readline() - 1;
+                $clienteEscolhido = $clientes[$indiceCliente];
+
+                echo "Digite a data de início do aluguel: ";
+                $dataInicio = readline();
+
+                $novoAluguel = new Aluguel($clienteEscolhido, $veiculoEscolhido, $dataInicio);
+                $alugueis[] = $novoAluguel;
+
+                echo "\nAluguel registrado com sucesso!\n";
+                $novoAluguel->exibirDetalhes();
+
+
+                echo "Digite o NÚMERO do método de pagamento: ";
+                echo "\n1. Cartão\n2. Boleto\n3.Pix\n";
+                $indicePag = (int) readline() - 1;
+                $metodoPagamento = $indicePag + 1; // 1: cartão, 2: boleto, 3: pix
+
+
+                if ($metodoPagamento === 1) {
+                    $pagamento = new PagamentoCartao($valorPreReserva, contaCliente: $clienteEscolhido->getConta());
+                    $sucessoPagamento = $pagamento->processar_pagamento();
+
+                } elseif ($metodoPagamento === 2) {
+                    $pagamento = new PagamentoBoleto($valorPreReserva, $clienteEscolhido->getConta());
+                    $sucessoPagamento = $pagamento->processar_pagamento();
+
+                } elseif ($metodoPagamento === 3) {
+                    $pagamento = new PagamentoPix($valorPreReserva, $clienteEscolhido->getConta());
+                    $sucessoPagamento = $pagamento->processar_pagamento();
+
+                } else {
+                    echo "Método de pagamento inválido.\n";
+                    break;
+                }
+            }
+
+        case 7:
+            if(empty($alugueis)){
+                echo "Ei querido, primeiro realiza o cadastro do aluguél.\n";
+                break;
+            }
+            else{
+                echo "_ Lista de Aluguéis _\n";
+                foreach ($alugueis as $aluguel) {
+                    $aluguel->exibirDetalhes();
+                }
+                break;
+            }
 
         case 8:
-            $sucessoPagamento = false;
-
-            echo "___ Pré Cadastro ___\n";
-            echo "Escolha um veículo da lista abaixo:\n";
-            foreach ($veiculos as $indice => $veiculo) {
-                echo ($indice + 1) . ". ";
-                $veiculo->exibirDetalhes();
+            if(empty($clientes)){
+                echo "Realiza o cadastro de cliente!\n";
+                break;
             }
-
-            echo "Digite o NÚMERO do veículo: ";
-            $indiceVeiculo = (int) readline() - 1;
-            $veiculoEscolhido = $veiculos[$indiceVeiculo];
-            $valorPreReserva = $veiculoEscolhido->getValor();
-            echo "\nEscolha um cliente da lista abaixo:\n";
-            foreach ($clientes as $indice => $cliente) {
-                echo ($indice + 1) . ". ";
-                $cliente->exibirDetalhes();
+            elseif(empty($veiculos)){
+                echo "Realiza o cadastro do veiculo!\n";
+                break;
             }
-            echo "Digite o NÚMERO do cliente: ";
-            $indiceCliente = (int) readline() - 1;
-            $clienteEscolhido = $clientes[$indiceCliente];
-            
-            echo "Digite o NÚMERO do método de pagamento: ";
-            echo "\n1. Cartão\n2. Boleto\n3.Pix\n";
-            $indicePag = (int) readline() - 1;
-            $metodoPagamento = $indicePag + 1; // 1: cartão, 2: boleto, 3: pix
+            else{
+                $sucessoPagamento = false;
 
-
-            if ($metodoPagamento === 1) {
-                $pagamento = new PagamentoCartao($valorPreReserva, $clienteEscolhido->getConta());
-                $sucessoPagamento = $pagamento->processar_pagamento();
-
-            } elseif ($metodoPagamento === 2) {
-                $pagamento = new PagamentoBoleto($valorPreReserva, $clienteEscolhido->getConta());
-                $sucessoPagamento = $pagamento->processar_pagamento();
-
-            } elseif ($metodoPagamento === 3) {
-                $pagamento = new PagamentoPix($valorPreReserva, $clienteEscolhido->getConta());
-                $sucessoPagamento = $pagamento->processar_pagamento();
-
-            } else {
-                echo "Método de pagamento inválido.\n";
-            break;
-            }
-            
-            echo "Digite a data limite de pagamento (Prazo): ";
-            $dataLimite = readline();
-
-            $novaPreReserva = new PreReserva($clienteEscolhido, $veiculoEscolhido, $dataLimite, $metodoPagamento);
-
-            if ($sucessoPagamento) {
-                echo "\n-------------------------------------------------\n";
-                echo "Digite a data SIMULADA em que o pagamento foi efetuado: ";
-                $dataPagamentoSimulada = readline();
-                echo "-------------------------------------------------\n";
-                $novaPreReserva->marcarComoPaga($dataPagamentoSimulada);
-                if ($novaPreReserva->estaDentroDoPrazo()) {
-
-                    echo "\nPagamento efetuado dentro do prazo ({$dataPagamentoSimulada}).\n";
-
-                    echo "Digite o PRAZO de aluguel: ";
-                    $prazoAluguel = readline(); 
-                    $aluguelFinal = new Aluguel($clienteEscolhido, $veiculoEscolhido, $prazoAluguel);
-                    $alugueis[] = $aluguelFinal; 
-
-                    $aluguelFinal->exibirDetalhes();
-                } else {
-                    echo "\nPagamento efetuado ({$dataPagamentoSimulada}), mas FORA DO PRAZO. Pré-Reserva CANCELADA.\n";
-                    $novaPreReserva->cancelaPreReserva(); 
+                echo "_ Pré Cadastro _\n";
+                echo "Escolha um veículo da lista abaixo:\n";
+                foreach ($veiculos as $indice => $veiculo) {
+                    echo ($indice + 1) . ". ";
+                    $veiculo->exibirDetalhes();
                 }
-            } else {
-                echo "\nFalha no pagamento. Pré-reserva não efetivada.\n";
-            }
 
-            $listaPreReserva[] = $novaPreReserva;
-            $novaPreReserva->exibirDetalhes();
+                echo "Digite o NÚMERO do veículo: ";
+                $indiceVeiculo = (int) readline() - 1;
+                $veiculoEscolhido = $veiculos[$indiceVeiculo];
+                $valorPreReserva = $veiculoEscolhido->getValor();
+                echo "\nEscolha um cliente da lista abaixo:\n";
+                foreach ($clientes as $indice => $cliente) {
+                    echo ($indice + 1) . ". ";
+                    $cliente->exibirDetalhes();
+                }
+                echo "Digite o NÚMERO do cliente: ";
+                $indiceCliente = (int) readline() - 1;
+                $clienteEscolhido = $clientes[$indiceCliente];
+                
+                echo "Digite o NÚMERO do método de pagamento: ";
+                echo "\n1. Cartão\n2. Boleto\n3.Pix\n";
+                $indicePag = (int) readline() - 1;
+                $metodoPagamento = $indicePag + 1; // 1: cartão, 2: boleto, 3: pix
+
+
+                if ($metodoPagamento === 1) {
+                    $pagamento = new PagamentoCartao($valorPreReserva, $clienteEscolhido->getConta());
+                    $sucessoPagamento = $pagamento->processar_pagamento();
+
+                } elseif ($metodoPagamento === 2) {
+                    $pagamento = new PagamentoBoleto($valorPreReserva, $clienteEscolhido->getConta());
+                    $sucessoPagamento = $pagamento->processar_pagamento();
+
+                } elseif ($metodoPagamento === 3) {
+                    $pagamento = new PagamentoPix($valorPreReserva, $clienteEscolhido->getConta());
+                    $sucessoPagamento = $pagamento->processar_pagamento();
+
+                } else {
+                    echo "Método de pagamento inválido.\n";
+                break;
+                }
+                
+                echo "Digite a data limite de pagamento (Prazo): ";
+                $dataLimite = readline();
+
+                $novaPreReserva = new PreReserva($clienteEscolhido, $veiculoEscolhido, $dataLimite, $metodoPagamento);
+
+                if ($sucessoPagamento) {
+                    echo "\n-------------------------------------------------\n";
+                    echo "Digite a data SIMULADA em que o pagamento foi efetuado: ";
+                    $dataPagamentoSimulada = readline();
+                    echo "-------------------------------------------------\n";
+                    $novaPreReserva->marcarComoPaga($dataPagamentoSimulada);
+                    if ($novaPreReserva->estaDentroDoPrazo()) {
+
+                        echo "\nPagamento efetuado dentro do prazo ({$dataPagamentoSimulada}).\n";
+
+                        echo "Digite o PRAZO de aluguel: ";
+                        $prazoAluguel = readline(); 
+                        $aluguelFinal = new Aluguel($clienteEscolhido, $veiculoEscolhido, $prazoAluguel);
+                        $alugueis[] = $aluguelFinal; 
+
+                        $aluguelFinal->exibirDetalhes();
+                    } else {
+                        echo "\nPagamento efetuado ({$dataPagamentoSimulada}), mas FORA DO PRAZO. Pré-Reserva CANCELADA.\n";
+                        $novaPreReserva->cancelaPreReserva(); 
+                    }
+                } else {
+                    echo "\nFalha no pagamento. Pré-reserva não efetivada.\n";
+                }
+
+                $listaPreReserva[] = $novaPreReserva;
+                $novaPreReserva->exibirDetalhes();
+                
+                break;
+            }
             
-            break;
 
         case 9:
             echo "Saindo do sistema. Até logo!\n";
